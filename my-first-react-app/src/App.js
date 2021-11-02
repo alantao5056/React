29f11c9e9e91fd806c454f6dc29c665f9e46503a
alantao5056 <alantao5056@outlook.com>
2021-11-02 15:59:47 -0400

    added modal and eventlist

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,18 +1,57 @@
 import './App.css';
 import { useState } from 'react'
+import Title from "./components/Title"
+import Modal from "./components/Modal"
+import EventList from './components/EventList';
 
 function App() {
-  const [name, setName] = useState('mario')
+  const [showModal, setShowModal] = useState(false)
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
-  const handleClick = () => {
-    setName('luigi')
-    console.log(name)
+  console.log(showModal)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return event.id !== id
+      })
+    })
+    console.log(id)
   }
+
+  const handleClose = () => setShowModal(false)
+
+  const subtitle = "All the latest events in Marioland"
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      <Title title="Events in Your Area" subtitle={subtitle}/>
+
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents && <EventList events={events} handleClick={handleClick}/>}
+
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>Terms and Conditions</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam autem quod facilis odit aliquid vel id praesentium dignissimos asperiores maiores unde corrupti quidem tenetur ut ab quibusdam deleniti, quam qui.</p>
+      </Modal>}
+
+      <div>
+        <button onClick={() => setShowModal(true)}>Show Modal</button>
+      </div>
     </div>
   );
 }
