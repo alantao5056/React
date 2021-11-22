1693d8f21e0bd14ac2ae585d8298dcb4cce5fb52
alantao5056 <alantao5056@outlook.com>
2021-11-22 16:02:03 -0500

    finished tripago

--- tripago/src/App.js
+++ tripago/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import './App.css';
+import TripList from './components/TripList';
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true)
   return (
     <div className="App">
-      
+      <button onClick={() => setShowTrips(false)}>hide trips</button>
+      {showTrips && <TripList />}
     </div>
   );
 }
