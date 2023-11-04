e6cdc417b031c02c8f905e52f452e9492275f6fc
alantao5056 <alantaolr@outlook.com>
2023-11-03 20:08:33 -0400

    finished cooking-ninja

--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom'
 import { useTheme } from '../hooks/useTheme'
+import Trashcan from "../assets/trashcan.svg"
 
 // styles
 import './RecipeList.css'
+import { projectFirestore } from '../firebase/config'
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme()
 
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>
+  }
+
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete()
   }
 
   return (
@@ -19,6 +25,11 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className='delete'
+            src={Trashcan}
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -17,7 +17,7 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true)
 
-    projectFirestore.collection('recipes').doc(id).get().then((doc) => {
+    const unsub = projectFirestore.collection('recipes').doc(id).onSnapshot((doc) => {
       if (doc.exists) {
         setIsPending(false)
         setRecipe(doc.data())
@@ -26,7 +26,15 @@ export default function Recipe() {
         setError('Could not find that recipe')
       }
     })
+
+    return () => unsub();
   }, [id])
+
+  const handleClick = () => {
+    projectFirestore.collection('recipes').doc(id).update({
+      title: 'Something completely different'
+    })
+  }
 
   return (
     <div className={`recipe ${mode}`}>
@@ -40,6 +48,7 @@ export default function Recipe() {
             {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
